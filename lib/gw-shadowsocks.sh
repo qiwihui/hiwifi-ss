2f4d0eeef15615ef3112fda46e70490271db64ed
qiwihui <qwh005007@gmail.com>
2016-08-01 18:50:36 +0800

    极路由ss翻墙(老界面)

---
+++ lib/gw-shadowsocks.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+status() {
+	if [ `killall -0 ss-local >/dev/null 2>&1; echo $?` == 1 ] || [ `killall -0 gw_redsocks >/dev/null 2>&1; echo $?` == 1 ]
+	then
+		echo -n "stopped"
+	else
+		echo -n "running"
+	fi
+}
+
+case "$1" in
+	status)
+		status
+		;;
+esac
