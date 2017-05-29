41be488a0a9cde08dfd1998455e2da9a164f3e97
qiwihui <qwh005007@gmail.com>
2017-05-29 19:32:10 +0800

    更清楚描述

--- shadow.sh
+++ shadow.sh
@@ -2,44 +2,43 @@
 #
 echo 'Go!'
 sleep 2
-echo 'Building environment... 创建临时目录ing'
+echo '创建临时目录'
 echo ''
 mkdir /tmp/geewan
 cd /tmp/geewan
 echo 'Done! 成功创建临时目录!'
 echo ''
 sleep 3
-echo 'Downloading files... 下载插件ing'
+echo '下载插件'
 echo ''
 
 if [ $1 == "12515805s" ]; then
-echo ">1.2.5.15805s"
-curl -k https://raw.githubusercontent.com/qiwihui/hiwifi-ss/master/ss.12515805s.tar.gz -o ss.tar.gz
+    echo ">1.2.5.15805s"
+    curl -k https://raw.githubusercontent.com/qiwihui/hiwifi-ss/master/ss.12515805s.tar.gz -o ss.tar.gz
 else
-echo "<1.2.5.15805s"
-curl -k https://raw.githubusercontent.com/qiwihui/hiwifi-ss/master/ss.tar.gz -o ss.tar.gz
+    echo "<1.2.5.15805s"
+    curl -k https://raw.githubusercontent.com/qiwihui/hiwifi-ss/master/ss.tar.gz -o ss.tar.gz
 fi
 
 echo 'Done! 下载完成'
 echo ''
 sleep 3
-echo 'Installing... 安装ing'
 echo -n "备份系统文件...."
 
 if [ -f /usr/lib/lua/luci/view/admin_web/menu/menu_left.htm.ssbak ]; then
-    echo -e "...[\e[31m 备份已存在\e[0m]"
+    echo -e "...[\e[31m备份已存在\e[0m]"
 else
     cp -a /usr/lib/lua/luci/view/admin_web/menu/menu_left.htm /usr/lib/lua/luci/view/admin_web/menu/menu_left.htm.ssbak
-    echo -e "....[\e[32m 备份完成\e[0m]"
+    echo -e "....[\e[32m备份完成\e[0m]"
 fi
 
 echo '安装插件...'
 tar xzvf ss.tar.gz -C / >>/dev/null
-echo '...[\e[32m 安装成功\e[0m]'
+echo -e '...[\e[32m安装成功\e[0m]'
 
 echo ''
 sleep 3
-echo 'adding uninstall information... 准备删除临时文件...'
+echo '添加卸载信息...'
 echo ''
 echo '' >>/usr/lib/opkg/status
 echo 'Package: geewan-ss' >>/usr/lib/opkg/status
@@ -50,7 +49,8 @@ echo 'Architecture: ralink' >>/usr/lib/opkg/status
 echo 'Installed-Time: 1422509506' >>/usr/lib/opkg/status
 echo 'Auto-Installed: yes' >>/usr/lib/opkg/status
 echo '' >>/usr/lib/opkg/status
-echo 'cleanning temporary files... 清理临时文件ing'
+
+echo '清理临时文件...'
 echo ''
 if test -e /var/run/luci-indexcache; then
     rm /var/run/luci-indexcache && echo 'Done! 清理完成 ' && echo '';
