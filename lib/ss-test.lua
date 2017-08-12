64759f8e8e16705002128993f0c8aebf269d336a
SouHong <30280682+SouHong@users.noreply.github.com>
2017-08-12 14:36:04 +0800

    Update ss-test.lua

--- lib/ss-test.lua
+++ lib/ss-test.lua
@@ -5,7 +5,7 @@
 local host, port = "www.youtube.com", 443
 local socket = require("socket")
 local tcp = assert(socket.tcp())
-tcp:settimeout(2);
+tcp:settimeout(5);
 tcp:connect(host, port);
 tcp:send("GET / HTTP/1.1\n");
 local s, status, partial = tcp:receive()
