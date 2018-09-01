753b819b63e6131e992187cf4a08c4b31e896893
davinpeng <pengdavin@gmail.com>
2018-09-01 22:32:19 +0800

    issue#233 add execute permissions on the plugin-upgrade.sh

--- lib/plugin-upgrade.sh
+++ lib/plugin-upgrade.sh
@@ -4,7 +4,7 @@ check() {
     latest_version=`/usr/bin/curl -k https://api.github.com/repos/qiwihui/hiwifi-ss/releases/latest -s | grep 'tag_name' | awk '{ print $2 }' | sed s/\"//g | sed s/,//g`
     if [ `echo $?` == 1 ]
     then
-        echo -n "Can't check the latest version."
+        echo -n "Can't check the latest version!"
     else
         echo -n "$latest_version"
     fi
